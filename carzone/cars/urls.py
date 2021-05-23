dc89a72f4070d390607a12602b3ed61cd02a6e8a
Varun <varunjha2000@gmail.com>
2021-05-23 18:46:59 +0530

    pagination,detail page done

--- carzone/cars/urls.py
+++ carzone/cars/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.cars, name="cars" ),
+    path('<int:id>', views.car_detail, name="car_detail" ),
 ] 
--- carzone/cars/views.py
+++ carzone/cars/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from cars.models import Car
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
+all_cars=Car.objects.order_by('-created_date')
 def cars(request):
-    return render(request,"cars/cars.html")
+    paginator=Paginator(all_cars,2)
+    page=request.GET.get('page')
+    paged_cars=paginator.get_page(page)
+    
+    data={
+        'all_cars':paged_cars,
+    }
+    return render(request,"cars/cars.html",data)
+
+def car_detail(request,id):
+    single_car= get_object_or_404(Car,pk=id)
+    data={
+        "single_car":single_car,
+    }
+    return render(request,"cars/car_detail.html",data)
